4cfd11d8bfee821bcd2a3f6c7d04fd45eeebc082
Nathan Goldnadel <95369458+Akomice@users.noreply.github.com>
2023-12-16 15:26:29 +0100

    TP2 - Suppression code inutile

--- app/src/main/java/com/gzl/todo/MainActivity.kt
+++ app/src/main/java/com/gzl/todo/MainActivity.kt
@@ -10,16 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        /* val buttonOpenDetail = findViewById<Button>(R.id.button_open_detail)
-
-        // Définir un listener pour le clic sur le bouton
-        buttonOpenDetail.setOnClickListener {
-            // Créer l'intent pour démarrer DetailActivity
-            val intent = Intent(this, DetailActivity::class.java)
-            // Démarrer DetailActivity
-            startActivity(intent)
-        } */
     }
 }
