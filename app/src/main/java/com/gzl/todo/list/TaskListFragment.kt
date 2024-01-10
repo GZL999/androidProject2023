2a8efeda3c0773805f571c90731dd1aa2ae823f3
GZL99 <thegzl99@gmail.com>
2024-01-10 23:38:30 +0100

    Bug TP3 Q10 corrige

--- app/src/main/java/com/gzl/todo/list/TaskListFragment.kt
+++ app/src/main/java/com/gzl/todo/list/TaskListFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
